aad86436a2efba5d79d3ca87d314991be14907a0
jasonLapina <lapina.jason@gmail.com>
2023-06-26 18:22:21 +0800

    browse areas, browse by areas

--- pages/area/[area].js
+++ pages/area/[area].js
@@ -1,6 +1,59 @@
-import { Box } from "@chakra-ui/react";
-function AreaPage() {
-  return <Box></Box>;
+import { Box, Grid } from "@chakra-ui/react";
+
+import axiosInstance from "../../shared/axiosInstance";
+import MealItem from "../../components/Meals/MealItem";
+import MyHeading from "../../shared/UI/MyHeading";
+
+function AreaPage({ area, meals }) {
+  return (
+    <Box>
+      <MyHeading>{area} Meals</MyHeading>
+
+      <Grid
+        gridTemplateColumns='1fr 1fr 1fr'
+        justifyItems='center'
+        rowGap='40px'
+        columnGap='16px'
+      >
+        {meals.map((item) => (
+          <MealItem key={item.id} meal={item} />
+        ))}
+      </Grid>
+    </Box>
+  );
 }
 
 export default AreaPage;
+
+export async function getStaticProps({ params }) {
+  const { area } = params;
+
+  const { data } = await axiosInstance.get(`filter.php?a=${area}`);
+  const meals = data.meals.map((item) => {
+    return {
+      title: item.strMeal,
+      image: item.strMealThumb,
+      id: item.idMeal,
+      price: Number(item.idMeal.slice(0, 2)),
+    };
+  });
+
+  return {
+    props: {
+      meals,
+      area,
+    },
+  };
+}
+export async function getStaticPaths() {
+  const { data } = await axiosInstance.get("/list.php?a=list");
+
+  const paths = data.meals.map((item) => {
+    return { params: { area: item.strArea } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
--- pages/area/index.js
+++ pages/area/index.js
@@ -1,6 +1,65 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, Image, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+import areasData from "../../data/dummy-area.json";
+import MyHeading from "../../shared/UI/MyHeading";
+
 function Area() {
-  return <Box>area page</Box>;
+  // const areas = JSON.parse(areasData);
+  console.log(areasData);
+
+  return (
+    <Box>
+      <MyHeading>Areas</MyHeading>
+      <Grid
+        columnGap='16px'
+        rowGap='32px'
+        gridTemplateColumns='repeat(auto-fit, minmax(280px, 1fr))'
+      >
+        {areasData.map((item) => (
+          <Box
+            pos='relative'
+            cursor='pointer'
+            w='280px'
+            h='160px'
+            key={item.strArea}
+            role='group'
+            as={Link}
+            href={`/area/${item.strArea}`}
+          >
+            <Image
+              alt={item.strArea + "flag"}
+              h='100%'
+              w='100%'
+              src={item.image}
+              boxShadow='8px 8px bisque'
+              transition='all .4s'
+              _groupHover={{
+                filter: "brightness(.6)",
+              }}
+            />
+            <Text
+              bgColor='salmon'
+              fontSize='24px'
+              pos='absolute'
+              top='90%'
+              transform='translateY(-50%)'
+              px='8px'
+              transition='all .4s'
+              transformOrigin='left'
+              visibility='hidden'
+              _groupHover={{
+                visibility: "visible",
+                top: "50%",
+              }}
+            >
+              {item.strArea}
+            </Text>
+          </Box>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
 
 export default Area;
